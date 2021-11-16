6944a3c85299205aff4fb8655b9de9f7657e9911
MleMoe <i@mle.moe>
2021-11-16 17:39:22 +0800

    :pencil2: fix typos

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: (window as any).__POWERED_BY_QIANKUN__
-      ? process.env.VUE_APP_MiCRO_ROUTER
-      : "/",
-    name: "Home",
+      ? process.env.VUE_APP_MICRO_ROUTER
+      : '/',
+    name: 'Home',
     component: Home,
     children: [
       // 其它的路由都写到这里
       {
-        path: "/",
-        name: "Home",
+        path: '/',
+        name: 'Home',
         component: Home,
       },
       {
-        path: "/about",
-        name: "About",
+        path: '/about',
+        name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-          import(/* webpackChunkName: "about" */ "../views/About.vue"),
+          import(/* webpackChunkName: "about" */ '../views/About.vue'),
       },
     ],
   },
